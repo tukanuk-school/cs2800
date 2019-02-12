5b7e38f089c5046ab298a2a197c588e0909b59ed
Tukanuk <benjamin@idavidson.ca>
2019-02-12 10:21:45 -0500

    adding game flow scripting

--- david11_RockPaperScissor/Assets/Scripts/PaperRockScissor.cs
+++ david11_RockPaperScissor/Assets/Scripts/PaperRockScissor.cs
@@ -4,15 +4,60 @@ using UnityEngine;
 
 public class PaperRockScissor : MonoBehaviour
 {
+    [Header("Set in inspector")]
+    public int numRounds = 10;
+    public string playerOneInput;
+    public string playerTwoInput;
+    public int currentRound;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        // while (currentRound < numRounds)
+        // {
+            
+        // }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        getPlayerInput();
+
+        if (playerOneInput != "" && playerTwoInput != "")
+        {
+            currentRound++;
+            playerOneInput = "";
+            playerTwoInput = "";
+        }
     }
+
+    void getPlayerInput()
+    {
+        if (Input.GetKey(KeyCode.A))
+        {
+            playerOneInput = "paper";
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            playerOneInput = "rock";
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            playerOneInput = "scissors";
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            playerTwoInput = "paper";
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            playerTwoInput = "rock";
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            playerTwoInput = "scissors";
+        }
+    }
+
 }
